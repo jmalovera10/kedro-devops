a38288c37b2c72136d029f606f5657d876657f5f
Juan Manuel Lovera <jma.lovera10@uniandes.edu.co>
2021-10-15 09:48:47 -0500

    Install pywin with conda@

--- src/tests/pipelines/data_engineering/test_upper_caser.py
+++ src/tests/pipelines/data_engineering/test_upper_caser.py
@@ -1,17 +1,17 @@
 import pandas as pd
+import pytest
 
 from kedro_devops.pipelines.data_engineering.nodes.upper_caser import upper_caser
 
-
 class TestUpperCaser:
-    def should_upper_case_string_data(self):
+    def test_string_data(self):
         """Should upper case string data"""
         basic_data = pd.DataFrame({'names': ['juan', 'manuel', 'alberto']})
         output = upper_caser(basic_data)
-        
+
         assert output.equals(pd.DataFrame({'names': ['JUAN', 'MANUEL', 'ALBERTO']}))
 
-    def should_not_upper_case_number_data(self):
+    def test_number_data(self):
         """Should not upper case number data"""
         basic_data = pd.DataFrame({'numbers': [1, 2, 3]})
         output = upper_caser(basic_data)
