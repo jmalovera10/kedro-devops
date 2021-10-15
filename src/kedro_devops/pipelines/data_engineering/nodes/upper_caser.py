2de6557a3c4c7f3012b1700e7b8c35cf6bf1bbb9
Juan Manuel Lovera <jma.lovera10@uniandes.edu.co>
2021-10-15 10:38:31 -0500

    Successful pipeline

--- src/kedro_devops/pipelines/data_engineering/nodes/upper_caser.py
+++ src/kedro_devops/pipelines/data_engineering/nodes/upper_caser.py
@@ -2,4 +2,13 @@ import pandas as pd
 
 
 def upper_caser(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Transforms a dataframe string data to upper case.
+
+    Args:
+        data (pd.DataFrame): Raw data to be transformed
+
+    Returns:
+        pd.DataFrame: A upper case string dataframe
+    """
     return data.applymap(lambda x: x.upper())
