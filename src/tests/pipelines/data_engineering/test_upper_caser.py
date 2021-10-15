77c83b225478d30faa6b1e222f0bbb001316fdd4
Juan Manuel Lovera <jma.lovera10@uniandes.edu.co>
2021-10-15 09:41:17 -0500

    Try package management with conda

--- src/tests/pipelines/data_engineering/test_upper_caser.py
+++ src/tests/pipelines/data_engineering/test_upper_caser.py
@@ -3,8 +3,17 @@ import pandas as pd
 from kedro_devops.pipelines.data_engineering.nodes.upper_caser import upper_caser
 
 
-def test_upper_caser():
-    basic_data = pd.DataFrame({'names': ['juan', 'manuel', 'alberto']})
-    output = upper_caser(basic_data)
-    
-    assert output.equals(pd.DataFrame({'names': ['JUAN', 'MANUEL', 'ALBERTO']}))
+class TestUpperCaser:
+    def should_upper_case_string_data(self):
+        """Should upper case string data"""
+        basic_data = pd.DataFrame({'names': ['juan', 'manuel', 'alberto']})
+        output = upper_caser(basic_data)
+        
+        assert output.equals(pd.DataFrame({'names': ['JUAN', 'MANUEL', 'ALBERTO']}))
+
+    def should_not_upper_case_number_data(self):
+        """Should not upper case number data"""
+        basic_data = pd.DataFrame({'numbers': [1, 2, 3]})
+        output = upper_caser(basic_data)
+        
+        assert output.equals(pd.DataFrame({'numbers': [1, 2, 3]}))
