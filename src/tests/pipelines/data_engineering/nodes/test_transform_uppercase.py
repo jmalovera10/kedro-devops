670df6357cf217deb692baaf7f401ee8bf58bc97
Juan Manuel Lovera <jma.lovera10@uniandes.edu.co>
2021-11-08 09:12:12 -0500

    Fix test

--- src/tests/pipelines/data_engineering/nodes/test_transform_uppercase.py
+++ src/tests/pipelines/data_engineering/nodes/test_transform_uppercase.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import json
+from requests.models import Response
+from pytest import MonkeyPatch
 
 from kedro_devops.pipelines.data_engineering.nodes.transform_uppercase import (
     transform_uppercase,
@@ -6,10 +9,20 @@ from kedro_devops.pipelines.data_engineering.nodes.transform_uppercase import (
 
 
 class TestTransformUppercase:
-    def test_transform_string(self):
+    def test_transform_string(self, monkeypatch: MonkeyPatch):
         """
         should return a upper case string for a string dataframe
         """
-        t_dataframe = pd.DataFrame({"names": ["foo", "bar", "baz"]})
+        def mock_json():
+            return {"results": [
+                {"data": "test1"},
+                {"data": "test2"},
+                {"data": "test3"}
+                ]}
+      
+        t_dataframe = Response()
+        monkeypatch.setattr(t_dataframe, "json", mock_json)
+     
         output = transform_uppercase(t_dataframe)
-        assert output.equals(pd.DataFrame({"names": ["FOO", "BAR", "BAZ"]}))
+
+        assert output.equals(pd.DataFrame({"data": ["TEST1", "TEST2", "TEST3"]}))
